25959d1fcdf4ce8581faa8907e4f451f8118233a
Sajid Karim <22100253@lums.edu.pk>
2021-04-08 00:56:11 +0500

    Added front end design files

--- frontend/src/components/forms/ModeratorAppForm.js
+++ frontend/src/components/forms/ModeratorAppForm.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 // import fetch from 'cross-fetch';
 import { Editor } from "@tinymce/tinymce-react";
 import axios from 'axios';
-
+import styled from 'styled-components'
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
@@ -30,44 +30,124 @@ handleEditorChange(Body, editor) {
 
 handleSubmit = (event) =>{
   event.preventDefault();
-  const url = "api/accounts/modapps";
+  const url = "api/main/add_publication";
   const data = { Name:this.state.Name, Location:this.state.Location, Why:this.state.Why, Body:this.state.Body };
   
-  axios.post(`api/accounts/modapps`, { data })
-    .then(res => console.log(res))
+  axios.post(`api/main/add_publication`, { data })
+    .then(res => res.json())
     .catch(error => console.error('Error:', error))
     .then(response => console.log('Success', response));
   }
     
   render(){
     return(
-        <form onSubmit={this.handleSubmit}>
-        <span>Full Name*</span>
-        <input type="text" name="Name" onChange={this.handleChange} /><br/>
-        <span>Location*</span>
-        <input type="text" name="Location" onChange={this.handleChange} /><br/>
-        <span>Why do you want to be a moderator?*</span>
-        <input type="text" name="Why" onChange={this.handleChange} /><br/>
-        <span>Describe your qualifications.*</span>
-        <Editor
-          value={this.state.Body}
-          apiKey="dn8136u1fhyng3ughxdyzfw93m38430c67msp493v583itva"
-          init={{
-            height: 200,
-            width: 500,
-            plugins: "image",
-            toolbar: "bold italic image",
-            menubar: false,
-            toolbar_location: "bottom",
-          }}
-          onEditorChange={this.handleEditorChange}
-        />
-        <input type="submit" value="Submit" />
-        </form> );
+      <Container>
+        <Head>Moderator Application</Head>
+        <FormContainer>
+            <Form onSubmit={this.handleSubmit}>
+            <UserInfo>
+            <Span>Full Name*</Span>
+            <Span>Location*</Span>
+            <Input type="text" name="Name" onChange={this.handleChange} />
+            <Input type="text" name="Location" onChange={this.handleChange} />
+            <Span style={{paddingTop:"80px"}} >Why do you want to be a moderator?*</Span><br/>
+            <Input type="text" name="Why" style={{marginTop:"120px",marginBottom:"50px",width:"950px" ,height:"300px"}} onChange={this.handleChange} /><br/>
+            </UserInfo>
+            
+            <EditorContainer>
+              <Span>Describe your qualifications.*</Span>
+              <Editor
+                value={this.state.Body}
+                apiKey="dn8136u1fhyng3ughxdyzfw93m38430c67msp493v583itva"
+                init={{
+                  height: 600,
+                  width: 950,
+                  plugins: "image",
+                  toolbar: "bold italic image",
+                  menubar: false,
+                  toolbar_location: "bottom",
+                }}
+                onEditorChange={this.handleEditorChange}
+              />
+              <Submit type="submit" value="Submit" />
+            </EditorContainer>
+          </Form>
+        </FormContainer> 
+      </Container>
+        );
   }
 }
 
 export default App;
 
-const container = document.getElementById("app");
-render(<App />, container);
+const Container = styled.div`
+  margin-left:150px;
+  margin-right:150px;
+
+`
+const Head = styled.h3`
+  width:1040px;
+  height:50px;
+  margin-top:30px;
+  margin-left:5px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  color:white;
+  background: #03204C;
+  border-radius: 8px;
+`
+const FormContainer = styled.div`
+  width: 1050px;
+  height: 1200px;
+  margin-top:20px;
+  display:flex;
+  justify-content:space-between;
+  background:#DCF2F8;
+  border-radius: 16px;
+
+`
+const Form = styled.form`
+`
+const UserInfo = styled.div`
+  display:grid;
+  grid-template-rows:20px 60px 20px 200px;
+  grid-template-columns:500px 500px;
+  margin-top:20px;
+  padding-left:50px;
+`
+
+const Span = styled.span`
+  font-weight:bold;
+  font-size:20px;
+  
+`
+const Input = styled.input`
+  width: 400px;
+  height: 60px;
+  margin-top:10px;
+  background: #F9F7FC;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius:6px;
+  outline:none;
+  border:none;
+`
+
+const EditorContainer = styled.div`
+  margin-left:50px;
+  margin-top:230px;
+
+`
+const Submit = styled.input`
+  width:80px;
+  height:30px;
+  position:relative;
+  bottom:55px;
+  left:800px;
+  background-color:#03204C;
+  color:white;
+  z-index:2;
+  border-radius:6px;
+  font-size:15px;
+  font-weight:bold;
+`
